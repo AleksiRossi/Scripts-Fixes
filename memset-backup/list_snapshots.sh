3c08ea3e6e95d6689dd967359f9d0c2540186a41
AleksiRossi <rossi.aleksi@gmail.com>
2016-04-28 16:33:59 +0300

    Delete list_snapshots.sh

--- memset-backup/list_snapshots.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/opt/ma-shell/ma-shell-master/build/scripts-2.7/ma-shell.py -k b08c5bd2eb6c460c90742f68a842de24 server.snapshot_list storage_name msipssoaa1|egrep -w snapshot_path > /var/log/backup/snapshot_log
-
