c3a92120a5454be207439f37e7d2a63eb2e9c1d3
AleksiRossi <rossi.aleksi@gmail.com>
2016-05-02 17:14:24 +0300

    typos

--- oracle-sftp-import/transfer.sh
+++ oracle-sftp-import/transfer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 LOGFILE=/home/oracle/transfer.log
 ZIPFILE=/home/oracle/*.zip
-echo "$(date "+%m%d%Y %T") : starting file transfer" >> $LOGGILE 2>&1
+echo "$(date "+%m%d%Y %T") : starting file transfer" >> $LOGFILE 2>&1
 sftp "user"@"server":"filepath" >> $LOGFILE 2>&1
-echo "$(date "+%m%d%Y %T") : transfer complete, starting unzip" >> $LOGGILE 2>&1
-unzip -o $ZIPFILE >> $LOGGILE 2>&1
-echo "$(date "+%m%d%Y %T") : unzip complete, starting import" >> $LOGGILE 2>&1
-impdp dpump_user/"password" directory=dumpdir dumpfile="dumpfile" logfile=imp.log tables="user.table" table_exists_action=replace >> $LOGGILE 2>&1
-echo -e "$(date "+%m%d%Y %T") : import complete \n" >> $LOGGILE 2>&1
+echo "$(date "+%m%d%Y %T") : transfer complete, starting unzip" >> $LOGFILE 2>&1
+unzip -o $ZIPFILE >> $LOGFILE 2>&1
+echo "$(date "+%m%d%Y %T") : unzip complete, starting import" >> $LOGFILE 2>&1
+impdp dpump_user/"password" directory=dumpdir dumpfile="dumpfile" logfile=imp.log tables="user.table" table_exists_action=replace >> $LOGFILE 2>&1
+echo -e "$(date "+%m%d%Y %T") : import complete \n" >> $LOGFILE 2>&1
